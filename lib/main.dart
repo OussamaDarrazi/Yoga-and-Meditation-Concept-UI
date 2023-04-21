739ae905cd110f62e50ee3772287e997c201b06c
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2023-04-21 18:14:43 +0000

    added category tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Yoga and Meditation App',
       theme: ThemeData(
         fontFamily: "Plus Jakarta Sans",
@@ -38,7 +39,102 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        color: const Color(0xFF242424),
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 100,
+                  width: 80,
+                  decoration: BoxDecoration(
+                    color: myBlack,
+                    border: Border.all(
+                        color: Colors.white.withOpacity(0.1), width: 5),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                ),
+                RichText(
+                    text: TextSpan(children: <TextSpan>[
+                  TextSpan(
+                      text: "Top Guru's For\n",
+                      style: Theme.of(context).textTheme.displayLarge),
+                  TextSpan(
+                      text: "Yoga Exercices",
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayLarge!
+                          .copyWith(fontWeight: FontWeight.bold))
+                ])),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  
+                  Container(
+                    padding: const EdgeInsets.all(19),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: const Color.fromARGB(255, 112, 112, 112)),
+                      gradient: LinearGradient(colors: [myBlack, const Color.fromARGB(255, 82, 82, 82)], begin: Alignment.centerLeft, end: Alignment.centerRight),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Text(
+                      "All Type",
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(19),
+                    decoration: BoxDecoration(
+                      color: mySecondaryYellow,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Text(
+                      "Full Body",
+                      style: TextStyle(),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(19),
+                    decoration: BoxDecoration(
+                      color: mySecondaryPink,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Text(
+                      "Upper",
+                      style: TextStyle(),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(19),
+                    decoration: BoxDecoration(
+                      color: myPrimaryCyan,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Text(
+                      "Lower",
+                      style: TextStyle(),
+                    ),
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
