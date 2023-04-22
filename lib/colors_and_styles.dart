cfd807c626cca395515033cdb3ff9e5091f19d5d
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2023-04-22 18:00:56 +0000

    edit mySecondaryPink

--- lib/colors_and_styles.dart
+++ lib/colors_and_styles.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const Color myBlack = Color(0xFF242424);
 const Color myGrey = Color(0x80000000);
 const Color mySecondaryYellow = Color(0xFFFBF6B5);
-const Color mySecondaryPink = Color(0xFFFAE8FB);
+const Color mySecondaryPink = Color(0xFFFFDFDF);
 const Color mySecondaryCyan = Color(0xFFCCF4F2);
 const Color mySecondaryLime = Color(0xFFD7FFBE);
 //primary colors
