3fb79913bd59f2e18e6c371a26dec688ea9cbbab
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2023-04-24 20:10:45 +0100

    added skeleton for animated positionned containers

--- lib/screens/guru_screen.dart
+++ lib/screens/guru_screen.dart
@@ -4,9 +4,15 @@ import 'package:flutter/src/widgets/placeholder.dart';
 
 import '../colors_and_styles.dart';
 
-class GuruScreen extends StatelessWidget {
+class GuruScreen extends StatefulWidget {
   const GuruScreen({super.key});
 
+  @override
+  State<GuruScreen> createState() => _GuruScreenState();
+}
+
+class _GuruScreenState extends State<GuruScreen> {
+  bool infoClicked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +26,7 @@ class GuruScreen extends StatelessWidget {
                 height: 20,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   GestureDetector(
                     onTap: () {
@@ -104,27 +111,234 @@ class GuruScreen extends StatelessWidget {
                     child: OverflowBox(
                       maxHeight: 500,
                       alignment: Alignment.topLeft,
-                      child: Text("Yoga Exercises\nwith Anna\nJuliane", style: Theme.of(context).textTheme.displayLarge!.copyWith(fontWeight: FontWeight.bold),),
+                      child: Text(
+                        "Yoga Exercises\nwith Anna\nJuliane",
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayLarge!
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
                   Hero(
                     tag: "crab",
                     child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 300,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
                             image: AssetImage("assets/images/yoga 1.png"),
-                            fit: BoxFit.fitHeight
-                          ),
-                        ),
+                            fit: BoxFit.fitHeight),
                       ),
+                    ),
                   ),
-                  
                 ],
               )
             ]),
-          )
+          ),
+          AnimatedPositioned(
+            curve: decelerateEasing,
+            duration: const Duration(milliseconds: 500),
+            left: infoClicked ? 0 : 100,
+            top: infoClicked ? 180 : 620,
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (details.primaryDelta! < 0) {
+                  setState(() {
+                    infoClicked = true;
+                  });
+                } else if (details.primaryDelta! > 0) {
+                  setState(() {
+                    infoClicked = false;
+                  });
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.all(30),
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: mySecondaryCyan,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                  ),
+                ),
+                child: AnimatedCrossFade(
+                    firstChild: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "Total Time",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.black.withOpacity(.7)),
+                            ),
+                            const Text(
+                              "1 hour",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "Kcal burn",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.black.withOpacity(.7)),
+                            ),
+                            const Text(
+                              "550 kcal",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "Total Poses",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.black.withOpacity(.7)),
+                            ),
+                            const Text(
+                              "20 Poses",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 80,
+                        )
+                      ],
+                    ),
+                    secondChild: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Yoga Information",
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: mySecondaryPink,
+                              borderRadius: BorderRadius.circular(30)),
+                          child: Row(children: [
+                            Container(
+                              height: 60,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white,
+                                  image: const DecorationImage(
+                                      image: AssetImage(
+                                          "assets/images/misc images/yoga pose.png"))),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("20 Poses",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold)),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Text("Number of poses in yoga",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black.withOpacity(.5)))
+                              ],
+                            )
+                          ]),
+                        )
+                      ],
+                    ),
+                    crossFadeState: infoClicked
+                        ? CrossFadeState.showSecond
+                        : CrossFadeState.showFirst,
+                    duration: const Duration(milliseconds: 500)),
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            curve: decelerateEasing,
+            duration: const Duration(milliseconds: 500),
+            right: infoClicked ? 0 : 100,
+            top: infoClicked ? 560 : 710,
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (details.primaryDelta! < 0) {
+                  setState(() {
+                    infoClicked = true;
+                  });
+                } else if (details.primaryDelta! > 0) {
+                  setState(() {
+                    infoClicked = false;
+                  });
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.all(30),
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: AnimatedCrossFade(
+                  duration: const Duration(milliseconds: 500),
+                  crossFadeState: infoClicked? CrossFadeState.showSecond:CrossFadeState.showFirst,
+                  firstChild: Row(
+                    children: const [
+                      SizedBox(
+                        width: 100,
+                      ),
+                      Text(
+                        "Yoga and Guru\nDetails",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  secondChild: const Placeholder(),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+            decoration: const BoxDecoration(
+            color: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+              ),
+            ),
+            child: const Text(
+              "Buy \$30",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ))
         ],
       ),
     );
